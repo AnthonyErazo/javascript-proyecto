328e08d457400f8b009faf7efa3cdf1178fd8f9e
Anthony Erazo <anthonyerazo76@gmail.com>
2023-06-18 20:43:15 -0500

    mejora visual y de la interaccion, ventana modal y DOM

--- js/app.js
+++ js/app.js
@@ -1,119 +1,464 @@
 const producto=[
+    //Memorias Ram 
     {
-        id: "memoriaRam",
-        nombre: "Kington Memoria Ram 8gb DDr4 3600MHZ",
-        categoria: "Memoria Ram",
-        precio:17,
-        imagen:"./img/MemoriaRamKingston.jpg",
+        id: "memoriaRam01",
+        idModal:"01memoriaRam",
+        nombre: "MEMORIA 16GB DDR4 KINGSTON FURY BEAST BUS 3200MHZ BLACK",
+        categoria:{
+            nombre:"Memoria Ram",
+            id:"memoriaRam"
+        },
+        precio:46.50,
+        imagen:"./img/MemoriaRamFury3.jpg",
         cantidad:0
     },
     {
-        id: "procesador",
-        nombre: "Procesador INTEL CORE I9-10900K",
-        categoria: "Procesador",
-        precio:590,
+        id: "memoriaRam02",
+        nombre: "MEMORIA 8GB DDR3 KINGSTON BUS 1600GHZ",
+        categoria:{
+            nombre:"Memoria Ram",
+            id:"memoriaRam"
+        },
+        precio:39.00,
+        imagen:"./img/MemoriaRamKingston2.jpg",
+        cantidad:0
+    },
+    {
+        id: "memoriaRam03",
+        nombre: "MEMORIA 8GB DDR3L ADATA UDIMM BUS 1600MHZ",
+        categoria:{
+            nombre:"Memoria Ram",
+            id:"memoriaRam"
+        },
+        precio:30.00,
+        imagen:"./img/MemoriaRamAdata.jpg",
+        cantidad:0
+    },
+    {
+        id: "memoriaRam04",
+        nombre: "MEMORIA 8GB DDR4 KINGSTON FURY RENEGADE RGB BUS 3200MHz BLACK",
+        categoria:{
+            nombre:"Memoria Ram",
+            id:"memoriaRam"
+        },
+        precio:38.50,
+        imagen:"./img/MemoriaRamFury.jpg",
+        cantidad:0
+    },
+    {
+        id: "memoriaRam05",
+        nombre: "MEMORIA 16GB DDR5 KINGSTON FURY BEAST WHITE RGB BUS 5200MHz",
+        categoria:{
+            nombre:"Memoria Ram",
+            id:"memoriaRam"
+        },
+        precio:75.00,
+        imagen:"./img/MemoriaRamFury2.jpg",
+        cantidad:0
+    },
+    {
+        id: "memoriaRam06",
+        nombre: "MEMORIA 8GB DDR4 T-FORCE VULCAN Z TUF GAMING ALLIANCE BUS 3200MHz",
+        categoria:{
+            nombre:"Memoria Ram",
+            id:"memoriaRam"
+        },
+        precio:24.50,
+        imagen:"./img/MemoriaRamTForce.jpg",
+        cantidad:0
+    },
+    {
+        id: "memoriaRam07",
+        nombre: "MEMORIA 16GB DDR4 VIPER GAMING ELITE GRIS CON DISIPADOR BUS 2666Mhz",
+        categoria:{
+            nombre:"Memoria Ram",
+            id:"memoriaRam"
+        },
+        precio:85.50,
+        imagen:"./img/MemoriaRamViperGaming.jpg",
+        cantidad:0
+    },
+    {
+        id: "memoriaRam08",
+        nombre: "MEMORIA 16GB DDR4 XPG GAMMIX D10 BLACK BUS 3200MHz",
+        categoria:{
+            nombre:"Memoria Ram",
+            id:"memoriaRam"
+        },
+        precio:39.50,
+        imagen:"./img/MemoriaRamXPG.jpg",
+        cantidad:0
+    },
+    //Procesadores Intel
+    {
+        id: "procesador01",
+        nombre: "PROCESADOR INTEL CORE I9 10900K 3.70GHZ 20MB",
+        categoria:{
+            nombre:"Procesador",
+            id:"procesador"
+        },
+        precio:590.00,
         imagen:"./img/ProcesadorIntel.jpg",
         cantidad:0
     },
     {
-        id: "procesador",
-        nombre: "Procesador AMD RYZEN 5-5600G",
-        categoria: "Procesador",
-        precio:170,
+        id: "procesador02",
+        nombre: "PROCESADOR INTEL CELERON G6900 3.40GHZ ",
+        categoria:{
+            nombre:"Procesador",
+            id:"procesador"
+        },
+        precio:68.50,
+        imagen:"./img/ProcesadorIntel2.jpg",
+        cantidad:0
+    },
+    {
+        id: "procesador03",
+        nombre: "PROCESADOR INTEL PENTIUM GOLD G5420 3.8GHZ 4.0MB",
+        categoria:{
+            nombre:"Procesador",
+            id:"procesador"
+        },
+        precio:76.80,
+        imagen:"./img/ProcesadorIntel3.jpg",
+        cantidad:0
+    },
+    {
+        id: "procesador04",
+        nombre: "PROCESADOR INTEL CORE I3-10100F 3.6GHZ 6.0MB",
+        categoria:{
+            nombre:"Procesador",
+            id:"procesador"
+        },
+        precio:82.30,
+        imagen:"./img/ProcesadorIntel4.jpg",
+        cantidad:0
+    },
+    {
+        id: "procesador05",
+        nombre: "PROCESADOR INTEL CORE I3 10105F 3.7 GHZ 6MB",
+        categoria:{
+            nombre:"Procesador",
+            id:"procesador"
+        },
+        precio:87.80,
+        imagen:"./img/ProcesadorIntel5.jpg",
+        cantidad:0
+    },
+    {
+        id: "procesador06",
+        nombre: "PROCESADOR INTEL CORE I5 10400F 2.9GHZ - 12MB",
+        categoria:{
+            nombre:"Procesador",
+            id:"procesador"
+        },
+        precio:129.10,
+        imagen:"./img/ProcesadorIntel6.jpg",
+        cantidad:0
+    },
+    //Procesadores AMD
+    {
+        id: "procesador07",
+        nombre: "PROCESADOR AMD RYZEN 5 5600G 3.9GHz 16MB",
+        categoria:{
+            nombre:"Procesador",
+            id:"procesador"
+        },
+        precio:162.00,
         imagen:"./img/ProcesadorAmd.jpg",
         cantidad:0
     },
     {
-        id: "placaMadre",
-        nombre: "Placa Madre ASUS PRIME H610M-K D4",
-        categoria: "Placa madre",
-        precio:103,
-        imagen:"./img/PlacaMadreIntel.jpg",
+        id: "procesador08",
+        nombre: "PROCESADOR AMD RYZEN 3 3200G 3.6GHZ 6.0MB",
+        categoria:{
+            nombre:"Procesador",
+            id:"procesador"
+        },
+        precio:90.60,
+        imagen:"./img/ProcesadorAmd2.jpg",
         cantidad:0
     },
     {
-        id: "placaMadre",
-        nombre: "Placa Madre ASUS B650M-A",
-        categoria: "Placa Madre",
-        precio:230,
+        id: "procesador09",
+        nombre: "PROCESADOR AMD RYZEN 5 4500MPK 3.6GHZ",
+        categoria:{
+            nombre:"Procesador",
+            id:"procesador"
+        },
+        precio:129.10,
+        imagen:"./img/ProcesadorAmd3.jpg",
+        cantidad:0
+    },
+    {
+        id: "procesador10",
+        nombre: "PROCESADOR AMD RYZEN 3 PRO 4350G 3.8GHZ",
+        categoria:{
+            nombre:"Procesador",
+            id:"procesador"
+        },
+        precio:118.10,
+        imagen:"./img/ProcesadorAmd4.jpg",
+        cantidad:0
+    },
+    {
+        id: "procesador11",
+        nombre: "PROCESADOR AMD RYZEN 5 4600G 3.7GHZ",
+        categoria:{
+            nombre:"Procesador",
+            id:"procesador"
+        },
+        precio:134.60,
+        imagen:"./img/ProcesadorAmd5.jpg",
+        cantidad:0
+    },
+    //Placas madre Intel
+    {
+        id: "placaMadre01",
+        nombre: "MAINBOARD GIGABYTE H470M H DDR4 LGA 1200",
+        categoria:{
+            nombre:"Placa Madre",
+            id:"placaMadre"
+        },
+        precio:79.60,
+        imagen:"./img/PlacaMadreIntel2.jpg",
+        cantidad:0
+    },
+    {
+        id: "placaMadre02",
+        nombre: "MAINBOARD MSI B560M PRO-E LGA 1200",
+        categoria:{
+            nombre:"Placa Madre",
+            id:"placaMadre"
+        },
+        precio:90.60,
+        imagen:"./img/PlacaMadreIntel3.jpg",
+        cantidad:0
+    },
+    {
+        id: "placaMadre03",
+        nombre: "MAINBOARD MSI PRO H610M-G DDR4 LGA1700",
+        categoria:{
+            nombre:"Placa Madre",
+            id:"placaMadre"
+        },
+        precio:93.30,
+        imagen:"./img/PlacaMadreIntel4.jpg",
+        cantidad:0
+    },
+    {
+        id: "placaMadre04",
+        nombre: "MAINBOARD ASUS PRIME H610M-K D4",
+        categoria:{
+            nombre:"Placa Madre",
+            id:"placaMadre"
+        },
+        precio:115.30,
+        imagen:"./img/PlacaMadreIntel.jpg",
+        cantidad:0
+    },
+    //Placas madre AMD
+    {
+        id: "placaMadre05",
+        nombre: "MAINBOARD ASUS PRIME B650M-A WIFI",
+        categoria:{
+            nombre:"Placa Madre",
+            id:"placaMadre"
+        },
+        precio:241.90,
         imagen:"./img/PlacaMadreAmd.jpg",
         cantidad:0
     },
     {
-        id: "fuenteDePoder",
-        nombre: "Fuente COOLER MASTER MWE 750 V2 750W 80 PLUS GOLD",
-        categoria: "Fuente de Poder",
-        precio:120,
+        id: "placaMadre06",
+        nombre: "MAINBOARD MSI A320M-A PRO AM4",
+        categoria:{
+            nombre:"Placa Madre",
+            id:"placaMadre"
+        },
+        precio:54.80,
+        imagen:"./img/PlacaMadreAmd2.jpg",
+        cantidad:0
+    },
+    {
+        id: "placaMadre07",
+        nombre: "MAINBOARD MSI A520M-A PRO AM4",
+        categoria:{
+            nombre:"Placa Madre",
+            id:"placaMadre"
+        },
+        precio:76.80,
+        imagen:"./img/PlacaMadreAmd3.jpg",
+        cantidad:0
+    },
+    {
+        id: "placaMadre08",
+        nombre: "MAINBOARD GIGABYTE B450M DS3H V2 AM4",
+        categoria:{
+            nombre:"Placa Madre",
+            id:"placaMadre"
+        },
+        precio:90.60,
+        imagen:"./img/PlacaMadreAmd4.jpg",
+        cantidad:0
+    },
+    {
+        id: "placaMadre09",
+        nombre: "MAINBOARD GIGABYTE B550M K DDR4 AM4",
+        categoria:{
+            nombre:"Placa Madre",
+            id:"placaMadre"
+        },
+        precio:112.60,
+        imagen:"./img/PlacaMadreAmd5.jpg",
+        cantidad:0
+    },
+    //Fuentes de poder
+    {
+        id: "fuenteDePoder01",
+        nombre: "FUENTE COOLER MASTER MWE 750 V2 750W 80 PLUS GOLD",
+        categoria:{
+            nombre:"Fuente de Poder",
+            id:"fuenteDePoder"
+        },
+        precio:112.00,
         imagen:"./img/FuenteDePoder.jpg",
         cantidad:0
-    }
+    },
+    {
+        id: "fuenteDePoder02",
+        nombre: "FUENTE MSI MAG A650BN 650W 80 PLUS BRONZE",
+        categoria:{
+            nombre:"Fuente de Poder",
+            id:"fuenteDePoder"
+        },
+        precio:72.00,
+        imagen:"./img/FuenteDePoder2.jpg",
+        cantidad:0
+    },
+    {
+        id: "fuenteDePoder03",
+        nombre: "FUENTE ANTRYX B500 V3 500W BLACK",
+        categoria:{
+            nombre:"Fuente de Poder",
+            id:"fuenteDePoder"
+        },
+        precio:29.00,
+        imagen:"./img/FuenteDePoder3.jpg",
+        cantidad:0
+    },
+    {
+        id: "fuenteDePoder04",
+        nombre: "FUENTE KOLINK CORE 500W 80 PLUS WHITE",
+        categoria:{
+            nombre:"Fuente de Poder",
+            id:"fuenteDePoder"
+        },
+        precio:40.00,
+        imagen:"./img/FuenteDePoder4.jpg",
+        cantidad:0
+    },
+    {
+        id: "fuenteDePoder05",
+        nombre: "FUENTE GIGABYTE P550B 550W 80 PLUS BRONZE",
+        categoria:{
+            nombre:"Fuente de Poder",
+            id:"fuenteDePoder"
+        },
+        precio:55.50,
+        imagen:"./img/FuenteDePoder5.jpg",
+        cantidad:0
+    },
+    {
+        id: "fuenteDePoder06",
+        nombre: "FUENTE GIGABYTE P650G 650W 80 PLUS GOLD",
+        categoria:{
+            nombre:"Fuente de Poder",
+            id:"fuenteDePoder"
+        },
+        precio:77.00,
+        imagen:"./img/FuenteDePoder6.jpg",
+        cantidad:0
+    },
+    {
+        id: "fuenteDePoder07",
+        nombre: "FUENTE ANTRYX KIRIN 650W 80 PLUS BRONZE",
+        categoria:{
+            nombre:"Fuente de Poder",
+            id:"fuenteDePoder"
+        },
+        precio:61.50,
+        imagen:"./img/FuenteDePoder7.jpg",
+        cantidad:0
+    },
+    {
+        id: "fuenteDePoder08",
+        nombre: "FUENTE COOLER MASTER MWE 550 V2 550W 80 PLUS BRONZE ",
+        categoria:{
+            nombre:"Fuente de Poder",
+            id:"fuenteDePoder"
+        },
+        precio:65.00,
+        imagen:"./img/FuenteDePoder8.jpg",
+        cantidad:0
+    },
+    {
+        id: "fuenteDePoder09",
+        nombre: "FUENTE COOLER MASTER MWE V2 750W 80 PLUS BRONZE ",
+        categoria:{
+            nombre:"Fuente de Poder",
+            id:"fuenteDePoder"
+        },
+        precio:85.00,
+        imagen:"./img/FuenteDePoder9.jpg",
+        cantidad:0
+    },
+    {
+        id: "fuenteDePoder10",
+        nombre: "FUENTE COOLER MASTER V1100 SFX 1100W 80 PLUS PLATINUM FULL MODULAR",
+        categoria:{
+            nombre:"Fuente de Poder",
+            id:"fuenteDePoder"
+        },
+        precio:435.00,
+        imagen:"./img/FuenteDePoder10.jpg",
+        cantidad:0
+    },
 ];
-function anadir(nombre){
-    for(const p of producto){
-        if(p.nombre==nombre){
-            p.cantidad++;
-            alert(`${nombre} añadido correctamente!!`);
-        }
-    };
-}
-function quitar(nombre){
-    for(const p of producto){
-        if(p.nombre==nombre){
-            if(p.cantidad>0){
-                p.cantidad--;
-                alert(`${nombre} eliminado correctamente!!`);
-            }else{
-                alert(`${nombre} no ha sido añadido!!`);
-            }
-        }
-    };
-    
-}
-function mostrar(){
-    let cantidadTotal=0;
-    producto.forEach(p=>{cantidadTotal+=(p.cantidad*p.precio)});
-    if(cantidadTotal==0){
-        alert("No se ha seleccionado productos!!");
-    }else{
-        let texto=`Todos los productos: \n`;
-        producto.forEach(p=>{
-            if(p.cantidad>0){
-                texto+=(`${p.nombre} | Precio: $${p.precio} | Cantidad: ${p.cantidad}\n`);
-            }
-        });
-        texto+=`Cantidad Total: ${cantidadTotal}`;
-        if(confirm("¿Desea continuar con el pedido?")){
-            let nombre=prompt("Ingrese su nombre: ");
-            let apellido=prompt("Ingrese su apellido: ");
-            alert(`Factura:\nNombres: ${nombre}\nApellido: ${apellido}\n${texto}\n------------Gracias por elegirnos :)------------`);
-            producto.forEach(p=>{p.cantidad=0});
-        }
-    }
-}
 
-const productContainer=document.querySelector('#productContainer');
-const botnCategory=document.querySelectorAll(".categorybotn");
-const titleMain=document.querySelector("#titleMain");
+const productContainer=document.querySelector('#productContainer'),
+    botnCategory=document.querySelectorAll(".categorybotn"),
+    titleMain=document.querySelector("#titleMain"),
+    windowModal=document.getElementById('windowModal'),
+    windowContainer=document.getElementById('windowContainer'),
+    closeButton=document.getElementById('closeButton'),
+    productModal=document.querySelector('#productModal');
+let botnView=document.querySelectorAll(".tooltip"),
+    botnAdd=document.querySelectorAll(".botnAdd");
 
 function loadProducts(productSelect){
     productContainer.innerHTML="";
-    for(const p of productSelect){
+    productSelect.forEach(p=>{
         const div=document.createElement("div");
         div.classList.add("product");
         div.innerHTML=`
-            <img src="${p.imagen}" alt="${p.categoria}">
+            <div class="iconoCapa">
+                <img src="${p.imagen}" alt="${p.categoria.nombre}">
+                <div class="tooltip" id="${p.id}">
+                    <span class="tooltipTop">Vista rápida</span>
+                    <i class="fa-regular fa-eye"></i>
+                </div>
+            </div>
             <div class="productInfo">
                 <h2>${p.nombre}</h2>
-                <p>Precio: $${p.precio}</p> 
-                <p class="text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis excepturi ad, nam placeat fuga accusantium ab sed cupiditate corporis laborum dolor iure illum molestias! Fugit facilis neque excepturi voluptatem molestias.</p>
-            </div>
-            <div class="btns">
-                <button onclick="anadir('${p.nombre}')">Añadir</button>
-                <button onclick="quitar('${p.nombre}')">Quitar</button>
+                <p>Precio: $${p.precio}</p>
+                <button id="${p.id}" class="botnAdd">Añadir</button>
             </div>
         `;
         productContainer.append(div);
-    };
+    });
+    actualizarBotonesAgregar();
+    actualizarBotonesModal();
 }
 loadProducts(producto);
 
@@ -123,9 +468,9 @@ botnCategory.forEach(botn=>{
         e.currentTarget.classList.add("selected");
 
         if(e.currentTarget.id!="todos"){
-            const productCategory=producto.find(producto=>producto.id===e.currentTarget.id);
-            titleMain.innerHTML=productCategory.categoria;
-            const productsBotn=producto.filter(producto=>producto.id===e.currentTarget.id);
+            const productCategory=producto.find(producto=>producto.categoria.id===e.currentTarget.id);
+            titleMain.innerHTML=productCategory.categoria.nombre;
+            const productsBotn=producto.filter(producto=>producto.categoria.id===e.currentTarget.id);
             loadProducts(productsBotn);
         }else{
             titleMain.innerHTML="Todos los productos";
@@ -134,3 +479,64 @@ botnCategory.forEach(botn=>{
         
     });
 });
+
+
+
+function modalWindowView(e){
+    windowModal.style.display='flex';
+        productModal.innerHTML="";
+        producto.forEach(p=>{
+            if(e.currentTarget.id==p.id){
+                const div=document.createElement("div");
+                div.classList.add("viewProductModal");
+                div.innerHTML=`
+                <img src="${p.imagen}" alt="${p.categoria.nombre}">
+                <div>
+                    <h2>${p.nombre}</h2>
+                    <p>Precio: $${p.precio}</p>
+                    <button id="${p.idModal}" class="botnAdd">Añadir</button>
+                </div>
+                `;
+                productModal.append(div);
+            };
+        });
+        actualizarBotonesAgregar();
+}
+function actualizarBotonesModal(){
+    botnView=document.querySelectorAll(".tooltip");
+    botnView.forEach(botn=>{
+        botn.addEventListener('click',(e)=>modalWindowView(e));
+    });
+}
+function actualizarBotonesAgregar(){
+    botnAdd=document.querySelectorAll(".botnAdd");
+    botnAdd.forEach(botn=>{
+        botn.addEventListener('click',(e)=>agregarCarrito(e))
+    });
+}
+function agregarCarrito(e){
+    console.log(e.currentTarget.id);
+    for(let p of producto){
+        if(p.id==e.currentTarget.id){
+            p.cantidad++;
+            console.log(p.cantidad);
+        }else if(p.idModal==e.currentTarget.id){
+            p.cantidad++;
+            console.log(p.cantidad);
+        };
+    }
+}
+
+
+const closeWindow=()=>{
+    windowContainer.classList.add('close');
+
+    setTimeout(()=>{
+        windowContainer.classList.remove('close');
+        windowModal.style.display='none';
+    },500);
+};
+
+closeButton.addEventListener('click',()=>closeWindow());
+
+window.addEventListener('click',e=>e.target==windowModal&&closeWindow());
